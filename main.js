ae3d143faf47a52148715bf27bdcdc2cf1399c88
Kevin <kevin@eaucole.com>
2024-06-30 00:32:59 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
 // App entry point
+import * as THREE from 'three';
+import WebGL from 'three/addons/capabilities/WebGL.js';
+
+if ( WebGL.isWebGL2Available() ) {
+
+} else {
+	const warning = WebGL.getWebGL2ErrorMessage();
+	document.getElementById('container').appendChild( warning );
+}
