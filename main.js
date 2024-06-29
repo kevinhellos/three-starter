e5e6eaa0093c6388801f4f4f892e25c4f2158502
Kevin <kevin@eaucole.com>
2024-06-30 00:33:19 +0800

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,8 @@ import * as THREE from 'three';
 import WebGL from 'three/addons/capabilities/WebGL.js';
 
 if ( WebGL.isWebGL2Available() ) {
-
+    // Codes go below
+    
 } else {
 	const warning = WebGL.getWebGL2ErrorMessage();
 	document.getElementById('container').appendChild( warning );
